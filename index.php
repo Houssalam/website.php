ea57d9ca327668f70fffa736d39366dedd1ce721
Houssalam <123447826+Houssalam@users.noreply.github.com>
2023-05-31 11:10:41 +0200

    ajout website course of php

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="index.php" method="post">
+    <label>radius</label>
+    <input type="text" name="radius">
+    <!-- <label>y:</label>
+    <input type="text" name="y">
+    <label>z:</label>
+    <input type="text" name="z"> -->
+    <input type="submit" value="calculate">
+   </form>
+</body>
+</html>
+   
+<?php
+
+// fonction maths
+//  $x = $_POST["x"];
+//  $y = $_POST["y"];
+//  $z = $_POST["z"];
+//  $total = null;
+
+// //  $total = abs($x);
+// // $total = round($x);
+// // $total = floor($x);
+// //  $total = ceil($x);
+// // $total = sqrt($x);
+// // $total = pow($x, $y);
+// // $total = max($x, $y, $z);
+// // $total = pi();
+// $total = rand(1, 6);
+
+//  echo $total;
+
+//    $radius = $_POST ["radius"];
+//    $circumference = null;
+
+//    $circumference = 2 * pi() * $radius;
+//    $circumference = round($circumference, 2);
+
+//    $area = pi() * pow($radius, 2);
+//    $area = round($area, 2);
+
+//    $volume = 4/3 * pi() * pow($radius, 3);
+//    $volume = round($volume, 2);
+ 
+//    echo " circumference = {$circumference} "." cm <br>"; 
+//    echo "Area = {$area} "."cm^2 <br>";
+//    echo "Volume = {$volume} "." cm^3 <br>";
+
+// boulean
+    // $age = 101;
+    // if($age >= 100) {
+    //     echo "You are too old to enter this site";
+    // }
+    // elseif ($age >= 18) {
+    //     echo "You may enter this site";
+    // }
+    // elseif($age <= 0) {
+    //     echo "That wasn't a valid age";
+    // }
+  
+    // else {
+    //    echo "You must be 18+ to enter" ;
+    // }
+?>
+
